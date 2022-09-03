0163d7827587d1990359fa206f60af4cc2c819bf
Luka Jovanovski <Luka.Jovanovski@netcetera.com>
2022-09-03 19:22:34 +0200

    disable CORS policy

---
+++ src/main/java/com/ukim/finki/learn2cookbackend/configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.ukim.finki.learn2cookbackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
